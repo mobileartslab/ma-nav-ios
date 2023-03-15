8280b0f2ec88384c1d8bfe220d8f66125ae9e70b
Nick Sophinos <nick@wdeanmedical.com>
2023-03-15 12:16:50 -0400

    work on login screen

---
+++ ma-nav-ios/LoginView.swift
@@ -0,0 +1,76 @@
+import SwiftUI
+
+let lightGreyColor = Color(red: 239.0/255.0, green: 243.0/255.0, blue: 244.0/255.0, opacity: 1.0)
+
+struct LoginView: View {
+  @State var username: String = ""
+  @State var password: String = ""
+  
+  var isSignInButtonDisabled: Bool {
+    [username, password].contains(where: \.isEmpty)
+  }
+  
+  var body: some View {
+    VStack {
+      WelcomeText()
+      UserImage()
+      
+      TextField("Username", text: $username)
+        .padding()
+        .background(lightGreyColor)
+        .cornerRadius(5.0)
+        .padding(.bottom, 20)
+      
+      SecureField("Password", text: $password)
+        .padding()
+        .background(lightGreyColor)
+        .cornerRadius(5.0)
+        .padding(.bottom, 20)
+      
+      Button(action: {print("Button tapped")}) {
+        LoginButtonContent()
+      }.disabled(isSignInButtonDisabled)
+    }
+    .padding()
+
+  }
+}
+  
+struct WelcomeText : View {
+  var body: some View {
+    return Text("Navigator")
+      .font(.largeTitle)
+      .fontWeight(.semibold)
+      .padding(.bottom, 20)
+    }
+}
+
+struct UserImage : View {
+  var body: some View {
+    return Image("appLogo")
+      .resizable()
+      .aspectRatio(contentMode: .fill)
+      .frame(width: 150, height: 150)
+      .padding(.bottom, 75)
+  }
+}
+
+struct LoginButtonContent : View {
+  var body: some View {
+    return Text("LOGIN")
+      .font(.headline)
+      .foregroundColor(.white)
+      .padding()
+      .frame(width: 220, height: 60)
+      .background(Color.green)
+      .cornerRadius(15.0)
+  }
+}
+
+#if DEBUG
+struct LoginView_Previews: PreviewProvider {
+  static var previews: some View {
+    LoginView()
+  }
+}
+#endif
--- ma-nav-ios/ma_nav_iosApp.swift
+++ ma-nav-ios/ma_nav_iosApp.swift
@@ -1,20 +1,11 @@
-//
-//  ma_nav_iosApp.swift
-//  ma-nav-ios
-//
-//  Created by Nick Sophinos on 3/6/23.
-//
-
 import SwiftUI
 
 @main
-struct ma_nav_iosApp: App {
-    let persistenceController = PersistenceController.shared
+struct ma_com_iosApp: App {
 
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
-        }
+  var body: some Scene {
+    WindowGroup {
+      LoginView()
     }
+  }
 }
