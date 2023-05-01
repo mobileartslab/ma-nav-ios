efe2ce45ebda9aded2bb7ff3858b684c9e61a460
Nick Sophinos <nick@wdeanmedical.com>
2023-05-01 11:34:36 -0400

    work on mapping

--- ma-nav-ios/MainView.swift
+++ ma-nav-ios/MainView.swift
@@ -1,12 +1,10 @@
 import SwiftUI
+import MapKit
 
 struct MainView: View {
-  var body: some View { 
-    VStack {
-      Text("Main View")
-        .font(.largeTitle)
-        .fontWeight(.semibold)
-        .padding(.bottom, 20)
-    }
+  @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
+  
+  var body: some View {
+    Map(coordinateRegion: $region).frame(width: 400, height: 300)
   }
 }
